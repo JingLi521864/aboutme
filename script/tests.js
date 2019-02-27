33ddb85b1904e8138dee5a59b28892b264652335
JingLI <lijing0592@gmail.com>
2019-02-26 21:00:11 -0600

    ad

--- script/tests.js
+++ script/tests.js
@@ -1,16 +1,12 @@
 function mathMax(first, second) {
-    first = Number(document.getElementById("first").value)
-
-    second = Number(document.getElementById("second").value)
-
     value = Math.max(first, second);
+    return value
 
-    
 }
-QUnit.test("mathMax",function(assert){
-    assert.max(mathMax(-10,-20));
-    assert.max(mathMax(10,20));
-    assert.max(mathMax(-2,9));
-    assert.max(mathMax(100,20));
-    assert.max(mathMax(8,-9));
+QUnit.test("mathMax", function (assert) {
+    assert.equal(mathMax(-10, -20), -10, "Worked");
+    assert.equal(mathMax(10, 20), 20, "worked");
+    assert.equal(mathMax(-2, 9), 9, "worked");
+    assert.equal(mathMax(100, 20), 100, "worked");
+    assert.equal(mathMax(8, -9), 8, "worked");
 })
